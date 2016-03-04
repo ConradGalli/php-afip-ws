fb484522292c06ad8827c6ed64f65550bb25db03
Pablo Reyes <reyesoft@gmail.com>
2016-03-04 11:46:31 -0300

    initial commit: PSR-4 and tests ready

---
+++ tests/AfipclassTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * This class doesn't do much yet..
+ */
+class AfipclassTest extends PHPUnit_Framework_TestCase
+{
+    public function	testisValid()
+	{
+        $this->assertEquals(true, true);
+	}	
+}
